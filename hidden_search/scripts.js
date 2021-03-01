f26ab6c7e0a1a18fc2cd1dab60ad82513970bcaf
limaluca <lucaslimamesmo@gmail.com>
2021-03-01 18:37:56 -0300

    Page with a hidden input effect

---
+++ hidden_search/scripts.js
@@ -0,0 +1,9 @@
+const search = document.querySelector(".search");
+const button = document.querySelector(".button");
+const input = document.querySelector(".input");
+
+
+button.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+})
