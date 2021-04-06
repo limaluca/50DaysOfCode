472c96140e2859a7300b1495cf68406e79508b76
limaluca <lucaslimamesmo@gmail.com>
2021-04-06 19:28:50 -0300

    Working with high order functions and using timeout to delay stuff

--- choice_picker/scripts.js
+++ choice_picker/scripts.js
@@ -6,6 +6,13 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (event) => {
     createTags(event.target.value);
+
+    if (event.key == 'Enter') {
+        setTimeout(() => {
+                event.target.value = '';
+            }, 10) // wait 10 mili seconds
+        randomSelect()
+    }
 })
 
 function createTags(input) {
@@ -21,4 +28,48 @@ function createTags(input) {
         tagEl.innerText = tag;
         tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+    const times = 10; //times its going to highlight before stops
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highLightTag(randomTag)
+        setTimeout(() => {
+            unhighLightTag(randomTag);
+        }, 100)
+
+    }, 100); //every 100 miliseconds
+
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highLightTag(randomTag);
+
+        }, 100)
+
+    }, times * 100)
+
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+
+    return tags[Math.floor(Math.random() * tags.length)];
+
+
+}
+
+function highLightTag(tag) {
+    tag.classList.add("highlight")
+}
+
+function unhighLightTag(tag) {
+    tag.classList.remove("highlight")
 }
