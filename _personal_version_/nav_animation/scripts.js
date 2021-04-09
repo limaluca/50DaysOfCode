336f675435284badacf1cb029e99083c2a752e27
limaluca <lucaslimamesmo@gmail.com>
2021-04-09 16:00:22 -0300

    JS utility to hide the Nav, to be unckickable using css visibility property

--- _personal_version_/nav_animation/scripts.js
+++ _personal_version_/nav_animation/scripts.js
@@ -1,6 +1,15 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
+const navList = document.getElementById("listNav");
 
 toggle.addEventListener('click', () => {
     nav.classList.toggle('active')
+    if (navList.style.visibility == "hidden") {
+        navList.style.visibility = "visible"
+    } else {
+        setTimeout(() => {
+            navList.style.visibility = "hidden"
+        }, 700)
+
+    }
 })
