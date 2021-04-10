0f20ba906a8cc9872f518c65a6a83c1b2c5945ac
limaluca <lucaslimamesmo@gmail.com>
2021-04-10 10:10:23 -0300

    Fixing the last cup bug. Now it can be unfilled too

--- water_cup/scripts.js
+++ water_cup/scripts.js
@@ -3,18 +3,27 @@ const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained")
 
-
-
+updateBigCup() //calling here to the litters remained can appear
 smallCups.forEach((cup, index) => {
     cup.addEventListener("click", () => highlightCups(index))
 })
 
+
+
+
+
 function highlightCups(index) {
 
-
-    if (smallCups[index].classList.contains("full") && !smallCups[index].nextElementSibling.classList.contains("full")) {
+    if (smallCups[index].nextElementSibling !== null) {
+        if (smallCups[index].classList.contains("full") && !smallCups[index].nextElementSibling.classList.contains("full")) {
+            index--
+        }
+    } else if (smallCups[index].classList.contains("full")) {
         index--
     }
+
+
+
 
 
     smallCups.forEach((cup, index2) => {
@@ -26,6 +35,34 @@ function highlightCups(index) {
 
     })
 
+    updateBigCup()
 
+
+
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".small-cup.full").length;
+
+    const totalCups = smallCups.length;
+    const cupHeight = 330;
+
+
+    if (fullCups == 0) {
+        percentage.style.visibility = "hidden";
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = "visible";
+        percentage.style.height = `${fullCups/totalCups * cupHeight}px`
+        percentage.innerText = `${fullCups/totalCups *100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = "hidden";
+        remained.style.height = 0;
+    } else {
+        remained.style.visibility = "visible";
+        liters.innerText = `${(2- (250 * fullCups / 1000))}L`
+    }
 
 }
