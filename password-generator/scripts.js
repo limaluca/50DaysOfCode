4f8179a4e80020bec1f7333af4c49d9065562a7e
limaluca <lucaslimamesmo@gmail.com>
2021-05-11 20:03:42 -0300

    Working with logic to generate random password. Still need some fixes

--- password-generator/scripts.js
+++ password-generator/scripts.js
@@ -1,9 +1,66 @@
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const upperCaseEl = document.getElementById("uppercase")
+const lowerCaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const SymbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
+
+generateEl.addEventListener("click", () => {
+    const length = +lengthEl.value //the + transforms into number
+    const hasLower = lowerCaseEl.checked
+    const hasUpper = upperCaseEl.checked
+    const hasNumbers = numbersEl.checked
+    const hasSymbols = SymbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumbers, hasSymbols, length)
+})
+
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if (!password) { return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+})
+
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+
+    if (typesCount === 0) {
+        return ''
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
+}
+
 
 // the last number of each return stands for
 // the code of each char on the String Chart
